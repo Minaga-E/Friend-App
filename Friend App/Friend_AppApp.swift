a7694b9c46cd6f4dde80510b355379c90d2e0ed8
Minaga Ekanayake <minaeka221@gmail.com>
2023-08-17 08:50:34 +0400

    Initial Commit

---
+++ Friend App/Friend_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friend_AppApp.swift
+//  Friend App
+//
+//  Created by Minaga Ekanayake on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Friend_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
